912ee9e4818ce08b7c399d28dc710e550ad1da0c
jibril04Afaa <jibrilBA04@gmail.com>
2026-09-01 19:47:12 -0400

    basic led flasher

--- main/esp32-ble-controller.c
+++ main/esp32-ble-controller.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/ledc.h"
 
+#define LED_GPIO GPIO_NUM_2
+
+
+/* NOTE TO SELF: use ESP_ERROR_CHECK() where applicable */
 void app_main(void)
 {
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
 
+    for(;;)
+    {
+        gpio_set_level(LED_GPIO, 1);
+        vTaskDelay(pdMS_TO_TICKS(500));
+        gpio_set_level(LED_GPIO, 0);
+        vTaskDelay(pdMS_TO_TICKS(500));
+
+    }
 }
+ 
